c6611cf9b4cd94dcb3c6db38c028bce47366b594
Erfanmoulaee <erfanmoulaee1375@gmail.com>
2021-12-13 00:55:36 +0330

    js added

--- app.js
+++ app.js
@@ -1,3 +1,11 @@
+var openModal = document.querySelector(".btn");
+var closeBtn = document.querySelector("#closeBtn");
+var modal = document.querySelector(".modal");
 
+openModal.addEventListener("click", () => {
+  modal.style.display = "block";
+});
 
-
+closeBtn.addEventListener("click", () => {
+  modal.style.display = "none";
+});
